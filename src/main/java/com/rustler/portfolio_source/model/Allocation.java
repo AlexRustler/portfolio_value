cf714eb4ef6a19a922b25ae0e99d13780caad5e4
Denis Belyakov <belyakovdv@bcs.ru>
2019-06-18 05:53:58 +0300

    Add Model Classes

---
+++ src/main/java/com/rustler/portfolio_source/model/Allocation.java
@@ -0,0 +1,8 @@
+package com.rustler.portfolio_source.model;
+
+public class Allocation {
+    private String sector;
+    private Float assetValue;
+    private Float proportion;
+}
+
---
+++ src/main/java/com/rustler/portfolio_source/model/Stock.java
@@ -0,0 +1,6 @@
+package com.rustler.portfolio_source.model;
+
+public class Stock {
+    private String symbol;
+    private Integer volume;
+}
