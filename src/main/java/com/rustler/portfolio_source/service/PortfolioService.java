953027290bbd91ef1f832bf1383d2635b1af0236
Denis Belyakov <belyakovdv@bcs.ru>
2019-06-19 06:38:08 +0300

    Получение цены и сектора с IEXCloud

--- src/main/java/com/rustler/portfolio_source/service/PortfolioService.java
+++ src/main/java/com/rustler/portfolio_source/service/PortfolioService.java
@@ -3,14 +3,25 @@ package com.rustler.portfolio_source.service;
 import com.rustler.portfolio_source.model.Stock;
 import com.rustler.portfolio_source.model.Stocks;
 import com.rustler.portfolio_source.model.Value;
-import com.sun.scenario.effect.impl.sw.sse.SSEBlend_SRC_OUTPeer;
 import org.springframework.stereotype.Service;
+import pl.zankowski.iextrading4j.api.stocks.Company;
+import pl.zankowski.iextrading4j.api.stocks.Quote;
+import pl.zankowski.iextrading4j.client.IEXCloudClient;
+import pl.zankowski.iextrading4j.client.IEXCloudTokenBuilder;
+import pl.zankowski.iextrading4j.client.IEXTradingApiVersion;
+import pl.zankowski.iextrading4j.client.IEXTradingClient;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.CompanyRequestBuilder;
+import pl.zankowski.iextrading4j.client.rest.request.stocks.QuoteRequestBuilder;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
+
+
 @Service
 public class PortfolioService {
+
+    private static final String TOKEN = "" ;
 
     public Value calculateValue(Stocks stocks) {
         Value portfolio_value = new Value();
@@ -32,21 +43,27 @@ public class PortfolioService {
     }
 
     private Double getLatestPrice(String symbol) {
-        if (symbol.length()==3) {
-            return 3.0;
-        }
-        else {
-            return 2.0;
-        }
+        final IEXCloudClient iexTradingClient = IEXTradingClient.create(IEXTradingApiVersion.IEX_CLOUD_V1,
+                new IEXCloudTokenBuilder()
+                        .withPublishableToken(TOKEN)
+                        .build());
+        final Quote quote = iexTradingClient.executeRequest(new QuoteRequestBuilder()
+                .withSymbol(symbol)
+                .build());
+        return quote.getLatestPrice().doubleValue();
+
     }
 
     private String getSector(String symbol) {
-        if (symbol.length()==3) {
-            return "Technology";
-        }
-        else {
-            return "Healtcare";
-        }
+
+        final IEXCloudClient iexTradingClient = IEXTradingClient.create(IEXTradingApiVersion.IEX_CLOUD_V1,
+                new IEXCloudTokenBuilder()
+                        .withPublishableToken(TOKEN)
+                        .build());
+        final Company company = iexTradingClient.executeRequest(new CompanyRequestBuilder()
+                .withSymbol(symbol)
+                .build());
+        return company.getSector();
     }
 
     Map<String, Double> updateSectors(Map<String, Double> sectors, String sector, Double newValue) {
